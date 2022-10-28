4f082f002cc4acb9438280a6f310de3f99f71b57
sappysealsz <enchantercode@gmail.com>
2022-10-28 21:20:03 +0500

    feat: function to let users mint their tokens

--- contracts/contracts/Market.sol
+++ contracts/contracts/Market.sol
@@ -2,13 +2,13 @@
 
 pragma solidity ^0.8.9; 
 
-
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ERC721.sol";
+import "./ERC1155.sol";
 
-
-
-contract Market {
+contract Market is ReentrancyGuard {
     
     //Using Counters library to keep a track of minted NFT tokens
     using Counters for Counters.Counter;
@@ -17,6 +17,8 @@ contract Market {
     //Counter for token Ids to prevent overflows
     Counters.Counter private _erc721tokenIds;
     Counters.Counter private _erc721ItemsSold;
+
+    Counters.Counter private _erc1155tokenIds;
 
 
     //Listing fee will be the commission that market will charge users for selling their Items;
@@ -28,21 +30,34 @@ contract Market {
 
     //Market items will be the items being auctioned on the marketplace
     struct NFT_MarketItem {
-        uint256 tokenId,
-        address payable seller,
-        address payable owner,
-        uint256 price,
-        bool sold
+        address payable minter;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    struct sNFT_MarketItem {
+        address payable minter;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        uint256 supply;
     }
 
 
     //Mapping to store uint256 tokenId to struct MarketItems
     mapping(uint256 => NFT_MarketItem) private idToNFTs;
 
+    mapping(uint256 => sNFT_MarketItem) private idTosNFTs;
+
 
     //Events for market items creation
     event NFT_MarketItem_Created(
         uint256 indexed tokenId,
+        address indexed minter,
         address seller,
         address owner,
         uint256 price,
@@ -50,7 +65,17 @@ contract Market {
 
     );
 
+    event sNFT_MarketItem_Created(
+        uint256 indexed tokenId,
+        address indexed minter,
+        address seller,
+        address owner,
+        uint256 price,
+        uint256 supply
 
+    );
+
+    
     //Constructor sets deployer as owner
 
     constructor(){
@@ -85,7 +110,7 @@ contract Market {
         address nftContract, 
         string memory tokenURI
         )
-        public returns(uint256)
+        public nonReentrant returns(uint256)
         {
 
         _erc721tokenIds.increment();
@@ -94,9 +119,28 @@ contract Market {
         return ERC721(nftContract).mintERC721Token(current_itemId, msg.sender, tokenURI);
     }
 
+
+    /**
+     * @dev function lets user mint their supply of ERC1155 Token
+     */
+
+
+    function createERC1155Token(
+        address nftContract, 
+        string memory tokenURI,
+        uint256 amount
+        )
+        public nonReentrant returns(uint256)
+        {
+
+        _erc1155tokenIds.increment();
+        uint256 current_itemId = _erc1155tokenIds.current();
+
+        return ERC1155(nftContract).createSupply(msg.sender, tokenURI, current_itemId, amount, "0x");
+    }
     /**
      * @dev function lets user create market items for sale
-     */
+     
 
     function create_NFT_MarketItem(
         address nftContract,
@@ -127,5 +171,5 @@ contract Market {
     //Implement a way of checking the minted erc721 items that are not up for sale 
 
 
-
+*/
 }
